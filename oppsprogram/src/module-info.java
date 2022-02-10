f5a432fae3e61035fcf36d0965edd0c96a48eb69
VAISHNAVI <VAISHNAVI@DESKTOP-F7T9MQT>
2022-02-10 12:45:45 +0530

    opps programs

---
+++ oppsprogram/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author VAISHNAVI
+ *
+ */
+module oppsprogram {
+}
---
+++ oppsprogram/src/oppsprogram/AcessModifier.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package oppsprogram;
+
+/**
+ * @author VAISHNAVI
+ *
+ */
+public class AcessModifier 
+{
+	int nos;
+	float hrs;
+	int noc;
+	void displayStudents()
+	{ 
+		System.out.println("number of students");
+	}
+	void displayHours()
+	{
+		System.out.println("number of hours");
+	}
+	void displayClass()
+	{
+		System.out.println("number of class");
+		
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) 
+	{
+		AcessModifier am= new AcessModifier();
+		am.displayStudents();
+		// TODO Auto-generated method stub
+
+	}
+
+}
